ecccf572f81e759eff674ede92f2c2d83a3ad610
Tohir Arapjonov <toxirarapjonov@gmail.com>
2024-06-15 15:26:42 +0500

    svsdbdgd

---
+++ index.js
@@ -0,0 +1,71 @@
+// 1 - vazifa
+
+// alert (5 % 2)
+
+// 2 - vazifa
+
+// let taxminam = Math.floor(Math.random () * 10);
+// alert(`${taxminam}`)
+
+// 3 - vazifa
+
+// let random = Math.floor("12.510 ");
+// alert(` ${random}`);
+
+// 4 - vazifa;
+
+// function getlength(text) {
+//     return text.length;
+// }
+
+// let uzunlik = getlength("Toxir"); // 5
+// alert(uzunlik);
+
+// 5 - vazfia
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("MARS IT SCHOOL");
+// }
+
+//  6 - vazifa
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ];
+// let ism = harflar[18] + harflar[13] + harflar [6] + harflar [7] + harflar [16];
+
+// console.log(ism)
+
+// 7 - vazifa
+
+// let yosh = Number(prompt("Yoshingizni kiriting"));
+
+// if (yosh > 18) {
+//     console.log(Siz balog'atga yetgansiz!)
+// } else {
+//     console.log(Siz balog'atga yetmagansiz!)
+// }
+
+// 8 - vazifa
+
+// let username = prompt("Enter here your name");
+// let result = username.charAt(0).toUpperCase();
+// let result2 = username.slice(1).toLowerCase();
+// let result3 = result + result2;
+// let mainResult = result3.split("").reverse().join("");
+// console.log(mainResult);
+
+// // 10 - vazifa
+
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let toq = [];
+// let juft = [];
+
+// for (let i = 0; i < sonlar.length; i++) {
+//   const element = sonlar[i];
+//   if (element % 2 === 0) {
+//     juft.push(element);
+//   } else {
+//     toq.push(element);
+//   }
+// }
+
+// console.log(juft);
+// console.log(toq);
